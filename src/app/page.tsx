50e5121a2e471d6319ef5a95a051887bd533634f
Shivraj <shivrajsoni09022005@gmail.com>
2025-10-11 01:03:57 +0530

    keypad component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,25 @@ import BackgroundCircles from "@/components/background-circles-client";
 import Experience_Button from "@/components/experience_button";
 import ExperienceSection from "@/components/experience_section";
 import SongPlayer from "@/components/SongPlayer";
+import { Keypad, BackgroundBeams } from "@/components/ui/macbook-scroll";
 
 // proof of work page should be in the middle
 export default function Home() {
   return (
     <div className="relative min-h-screen flex-col bg-white dark:bg-black">
       <Header />
-      <BackgroundCircles title="Time is Reactive " variant="tertiary" />
+      <BackgroundCircles />
       <div className="relative z-10">
-        <div className="h-[100vh]" />
+        <div className="h-[60vh]" />
         <div className="container mx-auto px-4 py-16">
           <ExperienceSection />
         </div>
-        <div className="h-[60vh]" />
+        <div className="relative w-full flex justify-center items-center my-16">
+          <BackgroundBeams />
+          <div className="transform scale-[0.8] sm:scale-100 md:scale-[1.35]">
+            <Keypad />
+          </div>
+        </div>
       </div>
       <SongPlayer />
       <Footer />
---
+++ src/components/ui/macbook-scroll.tsx
@@ -0,0 +1,517 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { cn } from "@/lib/utils";
+import {
+  IconBrightnessDown,
+  IconBrightnessUp,
+  IconCaretRightFilled,
+  IconCaretUpFilled,
+  IconChevronUp,
+  IconMicrophone,
+  IconMoon,
+  IconPlayerSkipForward,
+  IconPlayerTrackNext,
+  IconPlayerTrackPrev,
+  IconTable,
+  IconVolume,
+  IconVolume2,
+  IconVolume3,
+} from "@tabler/icons-react";
+import { IconSearch } from "@tabler/icons-react";
+import { IconWorld } from "@tabler/icons-react";
+import { IconCommand } from "@tabler/icons-react";
+import { IconCaretLeftFilled } from "@tabler/icons-react";
+import { IconCaretDownFilled } from "@tabler/icons-react";
+
+export const Keypad = () => {
+  const [activeKeys, setActiveKeys] = useState<string[]>([]);
+
+  useEffect(() => {
+    const sequence = [
+      { keys: ["cmd-left", "c"], duration: 300 },
+      { keys: [], duration: 300 },
+      { keys: ["cmd-left", "v"], duration: 300 },
+      { keys: [], duration: 2000 },
+    ];
+
+    let i = 0;
+    const loop = () => {
+      const { keys, duration } = sequence[i];
+      setActiveKeys(keys);
+      i = (i + 1) % sequence.length;
+      setTimeout(loop, duration);
+    };
+
+    const timeoutId = setTimeout(loop, 1500);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  return (
+    <div className="mx-1 h-full [transform:translateZ(0)] rounded-md bg-[#050505] p-1 [will-change:transform]">
+      {/* First Row */}
+      <div className="mb-[2px] flex w-full shrink-0 gap-[2px]">
+        <KBtn
+          className="w-10 items-end justify-start pb-[2px] pl-[4px]"
+          childrenClassName="items-start"
+        >
+          esc
+        </KBtn>
+        <KBtn>
+          <IconBrightnessDown className="h-[6px] w-[6px]" />
+          <span className="mt-1 inline-block">F1</span>
+        </KBtn>
+        <KBtn>
+          <IconBrightnessUp className="h-[6px] w-[6px]" />
+          <span className="mt-1 inline-block">F2</span>
+        </KBtn>
+        <KBtn>
+          <IconTable className="h-[6px] w-[6px]" />
+          <span className="mt-1 inline-block">F3</span>
+        </KBtn>
+        <KBtn>
+          <IconSearch className="h-[6px] w-[6px]" />
+          <span className="mt-1 inline-block">F4</span>
+        </KBtn>
+        <KBtn>
+          <IconMicrophone className="h-[6px] w-[6px]" />
+          <span className="mt-1 inline-block">F5</span>
+        </KBtn>
+        <KBtn>
+          <IconMoon className="h-[6px] w-[6px]" />
+          <span className="mt-1 inline-block">F6</span>
+        </KBtn>
+        <KBtn>
+          <IconPlayerTrackPrev className="h-[6px] w-[6px]" />
+          <span className="mt-1 inline-block">F7</span>
+        </KBtn>
+        <KBtn>
+          <IconPlayerSkipForward className="h-[6px] w-[6px]" />
+          <span className="mt-1 inline-block">F8</span>
+        </KBtn>
+        <KBtn>
+          <IconPlayerTrackNext className="h-[6px] w-[6px]" />
+          <span className="mt-1 inline-block">F8</span>
+        </KBtn>
+        <KBtn>
+          <IconVolume3 className="h-[6px] w-[6px]" />
+          <span className="mt-1 inline-block">F10</span>
+        </KBtn>
+        <KBtn>
+          <IconVolume2 className="h-[6px] w-[6px]" />
+          <span className="mt-1 inline-block">F11</span>
+        </KBtn>
+        <KBtn>
+          <IconVolume className="h-[6px] w-[6px]" />
+          <span className="mt-1 inline-block">F12</span>
+        </KBtn>
+        <KBtn>
+          <div className="h-4 w-4 rounded-full bg-gradient-to-b from-neutral-900 from-20% via-black via-50% to-neutral-900 to-95% p-px">
+            <div className="h-full w-full rounded-full bg-black" />
+          </div>
+        </KBtn>
+      </div>
+
+      {/* Second row */}
+      <div className="mb-[2px] flex w-full shrink-0 gap-[2px]">
+        <KBtn>
+          <span className="block">~</span>
+          <span className="mt-1 block">`</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">!</span>
+          <span className="block">1</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">@</span>
+          <span className="block">2</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">#</span>
+          <span className="block">3</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">$</span>
+          <span className="block">4</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">%</span>
+          <span className="block">5</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">^</span>
+          <span className="block">6</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">&</span>
+          <span className="block">7</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">*</span>
+          <span className="block">8</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">(</span>
+          <span className="block">9</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">)</span>
+          <span className="block">0</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">&mdash;</span>
+          <span className="block">_</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">+</span>
+          <span className="block"> = </span>
+        </KBtn>
+        <KBtn
+          className="w-10 items-end justify-end pr-[4px] pb-[2px]"
+          childrenClassName="items-end"
+        >
+          delete
+        </KBtn>
+      </div>
+
+      {/* Third row */}
+      <div className="mb-[2px] flex w-full shrink-0 gap-[2px]">
+        <KBtn
+          className="w-10 items-end justify-start pb-[2px] pl-[4px]"
+          childrenClassName="items-start"
+        >
+          tab
+        </KBtn>
+        <KBtn>
+          <span className="block">Q</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">W</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">E</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">R</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">T</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">Y</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">U</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">I</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">O</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">P</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">{`{`}</span>
+          <span className="block">{`[`}</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">{`}`}</span>
+          <span className="block">{`]`}</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">{`|`}</span>
+          <span className="block">{`\\`}</span>
+        </KBtn>
+      </div>
+
+      {/* Fourth Row */}
+      <div className="mb-[2px] flex w-full shrink-0 gap-[2px]">
+        <KBtn
+          className="w-[2.8rem] items-end justify-start pb-[2px] pl-[4px]"
+          childrenClassName="items-start"
+        >
+          caps lock
+        </KBtn>
+        <KBtn>
+          <span className="block">A</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">S</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">D</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">F</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">G</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">H</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">J</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">K</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">L</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">{`:`}</span>
+          <span className="block">{`;`}</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">{`"`}</span>
+          <span className="block">{`'`}</span>
+        </KBtn>
+        <KBtn
+          className="w-[2.85rem] items-end justify-end pr-[4px] pb-[2px]"
+          childrenClassName="items-end"
+        >
+          return
+        </KBtn>
+      </div>
+
+      {/* Fifth Row */}
+      <div className="mb-[2px] flex w-full shrink-0 gap-[2px]">
+        <KBtn
+          className="w-[3.65rem] items-end justify-start pb-[2px] pl-[4px]"
+          childrenClassName="items-start"
+        >
+          shift
+        </KBtn>
+        <KBtn>
+          <span className="block">Z</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">X</span>
+        </KBtn>
+        <KBtn isActive={activeKeys.includes("c")}>
+          <span className="block">C</span>
+        </KBtn>
+        <KBtn isActive={activeKeys.includes("v")}>
+          <span className="block">V</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">B</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">N</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">M</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">{`<`}</span>
+          <span className="block">{`,`}</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">{`>`}</span>
+          <span className="block">{`.`}</span>
+        </KBtn>
+        <KBtn>
+          <span className="block">{`?`}</span>
+          <span className="block">{`/`}</span>
+        </KBtn>
+        <KBtn
+          className="w-[3.65rem] items-end justify-end pr-[4px] pb-[2px]"
+          childrenClassName="items-end"
+        >
+          shift
+        </KBtn>
+      </div>
+
+      {/* sixth Row */}
+      <div className="mb-[2px] flex w-full shrink-0 gap-[2px]">
+        <KBtn className="" childrenClassName="h-full justify-between py-[4px]">
+          <div className="flex w-full justify-end pr-1">
+            <span className="block">fn</span>
+          </div>
+          <div className="flex w-full justify-start pl-1">
+            <IconWorld className="h-[6px] w-[6px]" />
+          </div>
+        </KBtn>
+        <KBtn className="" childrenClassName="h-full justify-between py-[4px]">
+          <div className="flex w-full justify-end pr-1">
+            <IconChevronUp className="h-[6px] w-[6px]" />
+          </div>
+          <div className="flex w-full justify-start pl-1">
+            <span className="block">control</span>
+          </div>
+        </KBtn>
+        <KBtn className="" childrenClassName="h-full justify-between py-[4px]">
+          <div className="flex w-full justify-end pr-1">
+            <OptionKey className="h-[6px] w-[6px]" />
+          </div>
+          <div className="flex w-full justify-start pl-1">
+            <span className="block">option</span>
+          </div>
+        </KBtn>
+        <KBtn
+          className="w-8"
+          childrenClassName="h-full justify-between py-[4px]"
+          isActive={activeKeys.includes("cmd-left")}
+        >
+          <div className="flex w-full justify-end pr-1">
+            <IconCommand className="h-[6px] w-[6px]" />
+          </div>
+          <div className="flex w-full justify-start pl-1">
+            <span className="block">command</span>
+          </div>
+        </KBtn>
+        <KBtn className="w-[8.2rem]"></KBtn>
+        <KBtn
+          className="w-8"
+          childrenClassName="h-full justify-between py-[4px]"
+        >
+          <div className="flex w-full justify-start pl-1">
+            <IconCommand className="h-[6px] w-[6px]" />
+          </div>
+          <div className="flex w-full justify-start pl-1">
+            <span className="block">command</span>
+          </div>
+        </KBtn>
+        <KBtn className="" childrenClassName="h-full justify-between py-[4px]">
+          <div className="flex w-full justify-start pl-1">
+            <OptionKey className="h-[6px] w-[6px]" />
+          </div>
+          <div className="flex w-full justify-start pl-1">
+            <span className="block">option</span>
+          </div>
+        </KBtn>
+        <div className="mt-[2px] flex h-6 w-[4.9rem] flex-col items-center justify-end rounded-[4px] p-[0.5px]">
+          <KBtn className="h-3 w-6">
+            <IconCaretUpFilled className="h-[6px] w-[6px]" />
+          </KBtn>
+          <div className="flex">
+            <KBtn className="h-3 w-6">
+              <IconCaretLeftFilled className="h-[6px] w-[6px]" />
+            </KBtn>
+            <KBtn className="h-3 w-6">
+              <IconCaretDownFilled className="h-[6px] w-[6px]" />
+            </KBtn>
+            <KBtn className="h-3 w-6">
+              <IconCaretRightFilled className="h-[6px] w-[6px]" />
+            </KBtn>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const KBtn = ({
+  className,
+  children,
+  childrenClassName,
+  backlit = true,
+  isActive = false,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+  childrenClassName?: string;
+  backlit?: boolean;
+  isActive?: boolean;
+}) => {
+  return (
+    <div
+      className={cn(
+        "[transform:translateZ(0)] rounded-[4px] p-[0.5px] [will-change:transform] transition-all duration-200",
+        isActive
+          ? "bg-blue-400/80 shadow-xl shadow-blue-500 ring-1 ring-blue-400/70 scale-[1.05]"
+          : backlit
+          ? "bg-white/[0.2] shadow-xl shadow-white hover:bg-white/[0.3] hover:shadow-2xl hover:shadow-white"
+          : ""
+      )}
+    >
+      <div
+        className={cn(
+          "flex h-6 w-6 items-center justify-center rounded-[3.5px] bg-[#0A090D]",
+          className,
+          isActive && "scale-[0.95]"
+        )}
+        style={{
+          boxShadow:
+            "0px -0.5px 2px 0 #0D0D0F inset, -0.5px 0px 2px 0 #0D0D0F inset",
+        }}
+      >
+        <div
+          className={cn(
+            "flex w-full flex-col items-center justify-center text-[5px] text-neutral-200",
+            childrenClassName,
+            backlit && "text-white"
+          )}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const OptionKey = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      fill="none"
+      version="1.1"
+      id="icon"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32 32"
+      className={className}
+    >
+      <rect
+        stroke="currentColor"
+        strokeWidth={2}
+        x="18"
+        y="5"
+        width="10"
+        height="2"
+      />
+      <polygon
+        stroke="currentColor"
+        strokeWidth={2}
+        points="10.6,5 4,5 4,7 9.4,7 18.4,27 28,27 28,25 19.6,25 "
+      />
+      <rect
+        id="_Transparent_Rectangle_"
+        className="st0"
+        width="32"
+        height="32"
+        stroke="none"
+      />
+    </svg>
+  );
+};
+
+export const BackgroundBeams = () => {
+  return (
+    <div className="absolute top-0 left-1/2 -translate-x-1/2 -z-10 w-full max-w-4xl h-full overflow-hidden">
+      <style>{`
+        @keyframes beam-shimmer {
+          0%, 100% { background-position: 0% 50%; }
+          50% { background-position: 100% 50%; }
+        }
+      `}</style>
+      <div
+        className="absolute inset-0"
+        style={{
+          maskImage: "radial-gradient(ellipse at center, white 5%, transparent 60%)",
+          WebkitMaskImage: "radial-gradient(ellipse at center, white 5%, transparent 60%)",
+        }}
+      >
+        <div
+          className="absolute inset-0 bg-[length:200%_100%] animate-[beam-shimmer_8s_linear_infinite]"
+          style={{
+            backgroundImage:
+              "linear-gradient(90deg, rgba(30, 64, 175, 0) 0%, rgba(30, 64, 175, 0.5) 25%, rgba(30, 64, 175, 0) 50%, rgba(30, 64, 175, 0.5) 75%, rgba(30, 64, 175, 0) 100%)",
+          }}
+        />
+      </div>
+    </div>
+  );
+};
